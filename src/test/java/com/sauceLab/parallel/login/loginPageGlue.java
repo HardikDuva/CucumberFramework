4a7f5c2e53c6a27974d6b0872507e311747bdfae
Hardik Duva <hardikduva@gmail.com>
2024-11-19 15:28:29 +0530

    Update BDD project

--- src/test/java/com/sauceLab/parallel/login/loginPageGlue.java
+++ src/test/java/com/sauceLab/parallel/login/loginPageGlue.java
@@ -22,28 +22,50 @@ public class loginPageGlue {
     public loginPageGlue(final SystemEnvironment sysEnv) {
         this.sauceLabObj = sysEnv;
     }
-    
+
     /**
-     * The user search the product
+     * Login with Valid username/email and password
      */
-    @When("^I try to login with valid email and password$")
+    @When("^I try to login with valid username/email and password$")
     public void loginWithValidCredentials() {
         sauceLabObj.setUsername(USERNAME);
 
         sauceLabObj.getLoginPage().enterUsername(USERNAME)
                 .enterPassword(PASSWORD)
                 .clickOnLoginInButton();
-
     }
 
     /**
-     * The user can see searched result for the product
+     * Login with In-valid username/email and password
      */
-    @When("^The user has successfully landed on the LMS home page$")
-    public void userIsOnHomePage() {
-        assertTrue("Product is not displayed in searched result page",
+    @When("^I try to login with In-valid username and password$")
+    public void loginWithInValidCredentials() {
+        sauceLabObj.setUsername(USERNAME);
+
+        sauceLabObj.getLoginPage().enterUsername(USERNAME)
+                .enterPassword(PASSWORD)
+                .clickOnLoginInButton();
+    }
+
+    /**
+     * The user can Log in successfully
+     */
+    @When("^The user should successfully landed on the LMS home page$")
+    public void userIsLoggedIn() {
+        assertTrue("User is not successfully landed on the LMS Home page",
                 sauceLabObj.getLoginPage()
                 .getPageURL().contains(HOMEPAGEURL));
+    }
+
+    /**
+     * The user should not successfully logged-in
+     */
+    @When("^The user should not successfully landed on the LMS home page$")
+    public void userIsNotLoggedIn() {
+        assertFalse("User is successfully landed on the LMS Home page even " +
+                        "invalid username & password",
+                sauceLabObj.getLoginPage()
+                        .getPageURL().contains(HOMEPAGEURL));
     }
 
 }
--- src/test/resources/TestCases/TEST-1.feature
+++ src/test/resources/TestCases/TEST-1.feature
@@ -1,13 +1,13 @@
 @full_regression
 @TEST-1
-@search_product
+@login
 
 Feature: TEST-1
  Background:
    Given I Initialize the framework with "TEST-1"
 
-  Scenario: As any user, I can search for products using search bar.
+  Scenario: As any user, I can login with valid username and password
     # This tests that user can login with valid username & password
 
-   When I try to login with valid email and password
-   Then The user has successfully landed on the LMS home page
+   When I try to login with valid username/email and password
+   Then The user should successfully landed on the LMS home page
